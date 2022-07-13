4f969ac27f6a68f44cf2160a2748a48132e08e03
Kelvin <kelvin.daquon.wade@gmail.com>
2022-07-13 11:54:23 -0500

    update

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class TestServlet {
+import javax.imageio.IIOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String firstName = req.getParameter("color");
+        res.setContentType("text/html");
+
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello "+firstName+" </h1>");
+        res.sendRedirect("/hello");
+    }
 }
